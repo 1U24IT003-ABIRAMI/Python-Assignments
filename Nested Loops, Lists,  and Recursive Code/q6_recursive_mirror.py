49be71a8d491aa4b06553c27b6547a71fc2d5164
ABIRAMI MANOHAR <manoabiramim@gmail.com>
2026-09-12 11:09:09 +0530

    Nested Loops, Lists,  and Recursive Code

---
+++ Nested Loops, Lists,  and Recursive Code/q6_recursive_mirror.py
@@ -0,0 +1,25 @@
+""" Question 6: recursive_mirror """
+"""
+Input: string s
+Output: string containing s and mirror of s
+        must be solved recursively
+"""
+def recursive_mirror(s):
+    if s=="":
+        return ""
+    else:
+        return s[0]+recursive_mirror(s[1:])+s[0]
+    
+
+""" Test 6 """
+def test_recursive_mirror():
+    print("Testing recursive_mirror...", end="")
+    assert(recursive_mirror("hello") == "helloolleh")
+    assert(recursive_mirror("wow") == "wowwow")
+    assert(recursive_mirror("code") == "codeedoc")
+    assert(recursive_mirror("recursion") == "recursionnoisrucer")
+    assert(recursive_mirror("") == "")
+    print("... done!")
+
+if __name__ == '__main__':
+    test_recursive_mirror()
